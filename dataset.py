e533dbc3da1a672fecf6cf36e084dc364e9fdeb5
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2024-01-07 15:01:34 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,6 +1,57 @@
 import numpy as np
 import torchvision.datasets as datasets
 
+class CatDogCIFAR10(datasets.CIFAR100):
+    base_folder = "cifar-10-batches-py"
+    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    filename = "cifar-10-python.tar.gz"
+    tgz_md5 = "c58f30108f718f92721af3b95e74349a"
+    train_list = [
+        ["data_batch_1", "c99cafc152244af753f735de768cd75f"],
+        ["data_batch_2", "d4bba439e000b95fd0a9bffe97cbabec"],
+        ["data_batch_3", "54ebc095f3ab1f0389bbae665268c751"],
+        ["data_batch_4", "634d18415352ddfa80567beed471001a"],
+        ["data_batch_5", "482c414d41f54cd18b22e5b47cb7c3cb"],
+    ]
+
+    test_list = [
+        ["test_batch", "40351d587109b95175f43aff81a1287e"],
+    ]
+    meta = {
+        "filename": "batches.meta",
+        "key": "label_names",
+        "md5": "5ff9c542aee3614f3951f8cda6e48888",
+    }
+    cls_num = 2
+    num_dogs = 50
+    num_cats = 5000
+
+    def __init__(self, root, imb_type='exp', imb_factor=0.01, rand_number=0, train=True,
+                 transform=None, target_transform=None, download=False):
+        super(CatDogCIFAR10, self).__init__(root, train, transform, target_transform, download)
+        cat_id, dog_id = self.classes.index('cat'), self.classes.index('dog')
+        imgs = list()
+        targets = list()
+        cat_cnt = 0
+        dog_cnt = 0
+        for idx, id in enumerate(self.targets):
+            if id == cat_id:
+                if cat_cnt <= CatDogCIFAR10.num_cats:
+                    imgs.append(self.data[idx])
+                    targets.append(0)
+                    cat_cnt += 1
+            elif id  == dog_id:
+                if dog_cnt <= CatDogCIFAR10.num_dogs:
+                    imgs.append(self.data[idx])
+                    targets.append(1)                    
+                    dog_cnt += 1
+        
+        assert len(targets) == CatDogCIFAR10.num_dogs + CatDogCIFAR10.num_cats
+        
+        self.data = imgs
+        self.targets = targets
+        self.classes = ['cats', 'dogs']
+        self.class_to_idx = {0: 'cats', 1: 'dogs'}
 
 
 class ImbalanceCIFAR10(datasets.CIFAR100):
