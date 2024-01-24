489dd28b86c3c7417d1585ab8917a167b147136c
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2024-01-24 20:58:04 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -157,7 +157,7 @@ def main():
         if is_best:
             y_raw = torch.tensor(y_raw, dtype=torch.float16)
             os.makedirs('./predictions', exist_ok=True)
-            torch.save(y_raw, f'./predictions/cifar10_{args.imb_factor}_pred.pt')
+            torch.save(y_raw, f'./predictions/cifar10lt_{args.imb_factor}_pred.pt')
             print('[INFO] Predictions are stored for best model')
 
         if epoch > 0 and epoch % args.save_every == 0:
